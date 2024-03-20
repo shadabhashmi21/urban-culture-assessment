6a47126960e74a3b3d9d30998a19b804bd519899
shadabhashmi21 <er.shadabhashmi@gmail.com>
2024-03-20 17:40:25 +0530

    added: image picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:urban_culture_assessment/resources/app_colors.dart';
 import 'package:urban_culture_assessment/screen/dashboard_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
--- lib/screen/routine_screen.dart
+++ lib/screen/routine_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
+import 'package:core/utils/core_utils.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:urban_culture_assessment/model/daily_skincare_model.dart';
 import 'package:urban_culture_assessment/resources/app_widgets.dart';
 
@@ -7,32 +12,60 @@ class RoutineScreen extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
+    final storageRef = FirebaseStorage.instance.ref();
+    Future getImageFromGallery() async {
+      final picker = ImagePicker();
+      final image = await picker.pickImage(source: ImageSource.gallery);
+
+      customPrint('Image -> $image');
+
+      final reference = storageRef.child('images');
+      final uploadTask = reference.putFile(File(image?.path ?? ''));
+
+      await uploadTask.whenComplete(() async{
+        String imageUrl = '';
+
+        try{
+          imageUrl = await reference.getDownloadURL();
+        }catch(onError){
+          customPrint('Error');
+        }
+
+        customPrint('Downloadable link -> $imageUrl');
+      });
+    }
+
     final list = [
       DailySkinCareModel(
         title: 'Cleanser',
         subtitle: 'Cetaphil Gentle Skin Cleanser',
         time: '8:00 PM',
-        onCameraIconTap: () {},),
+        onCameraIconTap: getImageFromGallery,
+      ),
       DailySkinCareModel(
         title: 'Toner',
         subtitle: 'Thayes Witch Hazel Toner',
         time: '8:02 PM',
-        onCameraIconTap: () {},),
+        onCameraIconTap: getImageFromGallery,
+      ),
       DailySkinCareModel(
         title: 'Moisturizer',
         subtitle: 'Kiehl\'s Ultra Facial Cream',
         time: '8:04 PM',
-        onCameraIconTap: () {},),
+        onCameraIconTap: getImageFromGallery,
+      ),
       DailySkinCareModel(
         title: 'Sunscreen',
         subtitle: 'Supgergoop Unseen Sunscreen SPF 40',
         time: '8:06 PM',
-        onCameraIconTap: () {},),
+        onCameraIconTap: getImageFromGallery,
+      ),
       DailySkinCareModel(
         title: 'Lip Balm',
         subtitle: 'Glossier Birthday Balm Dotcom',
         time: '8:08 PM',
-        onCameraIconTap: () {},),
+        onCameraIconTap: getImageFromGallery,
+      ),
     ];
 
     return Scaffold(
@@ -46,12 +79,12 @@ class RoutineScreen extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 10),
         child: ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: list.length,
-            itemBuilder: (final BuildContext context, final int index) {
-              final item = list[index];
-              return DailySkinCareRow(item: item);
-            },
+          padding: const EdgeInsets.all(8),
+          itemCount: list.length,
+          itemBuilder: (final BuildContext context, final int index) {
+            final item = list[index];
+            return DailySkinCareRow(item: item);
+          },
         ),
       ),
     );
