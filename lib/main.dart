37e9bd11f6baee5b8ee38e3c866e89da981e7530
shadabhashmi21 <shahbaz.matrixsystems@gmail.com>
2024-03-20 18:27:15 +0530

    fixed: firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:urban_culture_assessment/resources/app_colors.dart';
 import 'package:urban_culture_assessment/screen/dashboard_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
